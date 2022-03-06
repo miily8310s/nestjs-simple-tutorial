3907ad2621e221b5571b95937bf55d10eb7fb320
miruoon <rillakerika@gmail.com>
2022-03-07 08:03:34 +0900

    wip:exception filtersまで

---
+++ src/common/http-exception.filter.ts
@@ -0,0 +1,23 @@
+import {
+  ArgumentsHost,
+  Catch,
+  ExceptionFilter,
+  HttpException,
+} from '@nestjs/common';
+import { Request, Response } from 'express';
+
+@Catch(HttpException)
+export class HttpExceptionFilter implements ExceptionFilter<HttpException> {
+  catch(exception: HttpException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+    const request = ctx.getRequest<Request>();
+    const statusCode = exception.getStatus();
+
+    response.status(statusCode).json({
+      statusCode,
+      timestamp: new Date().toISOString(),
+      path: request.url,
+    });
+  }
+}
